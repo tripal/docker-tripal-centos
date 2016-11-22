082e57a4069768ed1f95886826a88df30efe690b
Ming Chen <ming.chen0919@gmail.com>
2016-11-22 11:55:57 -0500

    Update generate-elasticsearch-cluster-files.sh

--- generate-elasticsearch-cluster-files.sh
+++ generate-elasticsearch-cluster-files.sh
@@ -4,6 +4,6 @@ clusterName=$1
 
 sudo cp -r /etc/init.d/elasticsearch "/etc/init.d/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/etc/init.d/elasticsearch_"$clusterName
 sudo cp -r /var/lib/elasticsearch "/var/lib/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/var/lib/elasticsearch_"$clusterName
-sudo cp -r /etc/elasticsearch "/etc/elasticsearch_"$clustername && sudo chown -R elasticsearch:elasticsearch "/etc/elasticsearch_"$clustername
-sudo cp -r /var/log/elasticsearch "/var/log/elasticsearch_"$clustername && sudo chown -R elasticsearch:elasticsearch "/var/log/elasticsearch_"$clustername
-sudo cp -r /var/run/elasticsearch "/var/run/elasticsearch_"$clustername && sudo chown -R elasticsearch:elasticsearch "/var/run/elasticsearch_"$clustername
+sudo cp -r /etc/elasticsearch "/etc/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/etc/elasticsearch_"$clustername
+sudo cp -r /var/log/elasticsearch "/var/log/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/var/log/elasticsearch_"$clustername
+sudo cp -r /var/run/elasticsearch "/var/run/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/var/run/elasticsearch_"$clustername
