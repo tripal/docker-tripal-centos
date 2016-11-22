106d4f552938dc56700781dc6c07206fb252771c
Ming Chen <ming.chen0919@gmail.com>
2016-11-22 18:22:06 -0500

    Update generate-elasticsearch-cluster-files.sh

--- generate-elasticsearch-cluster-files.sh
+++ generate-elasticsearch-cluster-files.sh
@@ -3,13 +3,11 @@
 clusterName=$1
 
 sudo cp -r /etc/init.d/elasticsearch "/etc/init.d/elasticsearch_"$clusterName
-sed  "s|LOG_DIR=\"/var/log/elasticsearch\"|LOG_DIR=\"/var/log/elasticsearch_${clusterName}\"|g" "/etc/init.d/elasticsearch_"$clusterName && \\
-sed  "s|LOG_DIR=\"/var/lib/elasticsearch\"|LOG_DIR=\"/var/lib/elasticsearch_${clusterName}\"|g" "/var/lib/elasticsearch_"$clusterName && \\
-sed  "s|LOG_DIR=\"/etc/elasticsearch\"|LOG_DIR=\"/etc/elasticsearch_${clusterName}\"|g" "/etc/elasticsearch_"$clusterName && \\
-sed  "s|LOG_DIR=\"/var/log/elasticsearch\"|LOG_DIR=\"/var/log/elasticsearch_${clusterName}\"|g" "var/log/elasticsearch_"$clusterName && \\
-sed "s|/var/run/elasticsearch|/var/run/elasticsearch|g"
-
-
+sed  -i "s|LOG_DIR=\"/var/log/elasticsearch\"|LOG_DIR=\"/var/log/elasticsearch_${clusterName}\"|g" "/etc/init.d/elasticsearch_"$clusterName && \\
+  sed  -i "s|LOG_DIR=\"/var/lib/elasticsearch\"|LOG_DIR=\"/var/lib/elasticsearch_${clusterName}\"|g" "/var/lib/elasticsearch_"$clusterName && \\
+  sed  -i "s|LOG_DIR=\"/etc/elasticsearch\"|LOG_DIR=\"/etc/elasticsearch_${clusterName}\"|g" "/etc/elasticsearch_"$clusterName && \\
+  sed  -i "s|LOG_DIR=\"/var/log/elasticsearch\"|LOG_DIR=\"/var/log/elasticsearch_${clusterName}\"|g" "var/log/elasticsearch_"$clusterName && \\
+  sed  -i "s|PID_DIR=\"/var/run/elasticsearch\"|LOG_DIR=\"/var/run/elasticsearch_${clusterName}\"|g" "var/run/elasticsearch_"$clusterName
 
 sudo cp -r /var/lib/elasticsearch "/var/lib/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/var/lib/elasticsearch_"$clusterName
 sudo cp -r /etc/elasticsearch "/etc/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/etc/elasticsearch_"$clustername
