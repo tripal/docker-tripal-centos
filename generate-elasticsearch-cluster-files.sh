a886bad2290ebc275acbbea92dad4f978704e010
Ming Chen <ming.chen0919@gmail.com>
2016-11-22 11:56:39 -0500

    Update generate-elasticsearch-cluster-files.sh

--- generate-elasticsearch-cluster-files.sh
+++ generate-elasticsearch-cluster-files.sh
@@ -2,7 +2,7 @@
 
 clusterName=$1
 
-sudo cp -r /etc/init.d/elasticsearch "/etc/init.d/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/etc/init.d/elasticsearch_"$clusterName
+sudo cp -r /etc/init.d/elasticsearch "/etc/init.d/elasticsearch_"$clusterName 
 sudo cp -r /var/lib/elasticsearch "/var/lib/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/var/lib/elasticsearch_"$clusterName
 sudo cp -r /etc/elasticsearch "/etc/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/etc/elasticsearch_"$clustername
 sudo cp -r /var/log/elasticsearch "/var/log/elasticsearch_"$clusterName && sudo chown -R elasticsearch:elasticsearch "/var/log/elasticsearch_"$clustername
