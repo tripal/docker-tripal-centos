facb88b9d7827e597dc812abc0154d715a513294
MChen <ming.chen0919@gmail.com>
2016-11-25 02:27:26 -0500

    added default elasticsearch cluster

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,10 @@
 ## start postgresql
 sudo -u postgres pg_ctl start -D /var/lib/pgsql/data/ && sleep 15
 
-## start three of the five elasticsearch cluster
+## start the default elasticsearch cluster
+/etc/init.d/elasticsearch start && sleep 3
+
+## start three of the five custom elasticsearch cluster
 /etc/init.d/elasticsearch_myCluster01 start && sleep 3 # port=9201 
 /etc/init.d/elasticsearch_myCluster02 start && sleep 3 # port=9202
 /etc/init.d/elasticsearch_myCluster03 start && sleep 3 # port=9203
