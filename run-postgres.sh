28750ff7ac9fc7cc6075b5ed99b2dd5e671cd31b
MChen <ming.chen0919@gmail.com>
2016-12-10 03:55:39 -0500

    updated everything

--- run-postgres.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf /var/lib/pgsql/data/postmaster.pid
-sudo -u pg_ctl start -D /var/lib/pgsql/data/
-sleep 15
--- run-servers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf /run/httpd/* /tmp/httpd* && \
-  exec /usr/sbin/apachectl && \
-  sudo su - postgres && \
-  pg_ctl start -D /var/lib/pgsql/data/ && sleep 15 && sudo su
