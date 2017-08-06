f27e04a7dce0f10f3edb632d1b64c8567aa8437f
MChen <ming.chen0919@gmail.com>
2017-08-06 16:51:26 -0400

    updated add-elasticsearch-instance.sh

--- tripal-v3/add-elasticsearch-instance.sh
+++ tripal-v3/add-elasticsearch-instance.sh
@@ -2,13 +2,13 @@
 
 CLUSTER_NAME=$1
 NODE_NAME=$2
-PORT=$3
+HTTP_PORT=$3
 MINIMUM_MASTER_NODES=$4
 
 ## make a copy of init script of elasticsearch
 cp -r /etc/init.d/elasticsearch "/etc/init.d/elasticsearch_${NODE_NAME}"
 ## make a copy of log directory
-cp -r /var/log/elasticsearch "/var/log/elasticsearch_${NODE_NAME}" && chown -R elasticsearch:elasticsearch "/var/log/elasticsearch_${NODE_NAME}" 
+cp -r /var/log/elasticsearch "/var/log/elasticsearch_${NODE_NAME}" && chown -R elasticsearch:elasticsearch "/var/log/elasticsearch_${NODE_NAME}"
 ## make a copy of data path
 cp -r /var/lib/elasticsearch "/var/lib/elasticsearch_${NODE_NAME}" && chown -R elasticsearch:elasticsearch "/var/lib/elasticsearch_${NODE_NAME}"
 ## make a copy of configure file directory
@@ -17,26 +17,38 @@ cp -r /etc/elasticsearch "/etc/elasticsearch_${NODE_NAME}" && chown -R elasticse
 cp -r /var/run/elasticsearch "/var/run/elasticsearch_${NODE_NAME}" && chown -R elasticsearch:elasticsearch "/var/run/elasticsearch_${NODE_NAME}"
 
 ## associate the copy of init script of elasticsearch with the new log, data, configure and pid path
-sed  -i "s|LOG_DIR=\"/var/log/elasticsearch\"|LOG_DIR=\"/var/log/elasticsearch_${NODE_NAME}\"|g" "/etc/init.d/elasticsearch_${NODE_NAME}" 
+sed  -i "s|LOG_DIR=\"/var/log/elasticsearch\"|LOG_DIR=\"/var/log/elasticsearch_${NODE_NAME}\"|g" "/etc/init.d/elasticsearch_${NODE_NAME}"
 sed  -i "s|DATA_DIR=\"/var/lib/elasticsearch\"|DATA_DIR=\"/var/lib/elasticsearch_${NODE_NAME}\"|g" "/etc/init.d/elasticsearch_${NODE_NAME}"
 sed  -i "s|CONF_DIR=\"/etc/elasticsearch\"|CONF_DIR=\"/etc/elasticsearch_${NODE_NAME}\"|g" "/etc/init.d/elasticsearch_${NODE_NAME}"
 sed  -i "s|PID_DIR=\"/var/run/elasticsearch\"|PID_DIR=\"/var/run/elasticsearch_${NODE_NAME}\"|g" "/etc/init.d/elasticsearch_${NODE_NAME}"
 sed  -i "s|export ES_HEAP_SIZE|export ES_HEAP_SIZE=256m|g"  "/etc/init.d/elasticsearch_${NODE_NAME}"
 
 
+## build elasticsearch.yml file
 cd /etc/elasticsearch_"${NODE_NAME}"
-echo "cluster.name : ${CLUSTER_NAME}" > elasticsearch.yml
-echo "node.name : ${NODE_NAME}" >> elasticsearch.yml
-#echo "node.master : false" >> elasticsearch.yml
-#echo "node.data : true" >> elasticsearch.yml
-echo "path.data : /var/lib/elasticsearch_${NODE_NAME}" >> elasticsearch.yml
-echo "path.logs : /var/log/elasticsearch_${NODE_NAME}" >> elasticsearch.yml
-echo "network.host : 0.0.0.0" >> elasticsearch.yml
-echo "http.port : ${PORT}" >> elasticsearch.yml
-echo "bootstrap.memory_lock : true" >> elasticsearch.yml
-## no need to set the discovery.zen.ping.unicast.hosts when multiple nodes are in the same host.
-#echo "discovery.zen.ping.unicast.hosts: ['0.0.0.0', '0.0.0.0: `expr ${PORT} + 100`']" >> elasticsearch.yml
+echo '#----cluster name---------------' > elasticsearch.yml
+echo "cluster.name: ${CLUSTER_NAME}" >> elasticsearch.yml
+
+echo '#----node module settings-------' >> elasticsearch.yml
+echo "node.name: ${NODE_NAME}" >> elasticsearch.yml
+echo "node.master: true" >> elasticsearch.yml
+echo "node.data: true" >> elasticsearch.yml
+echo "path.data: /var/lib/elasticsearch_${NODE_NAME}" >> elasticsearch.yml
+echo "path.logs: /var/log/elasticsearch_${NODE_NAME}" >> elasticsearch.yml
+
+echo '#----network module settings--------' >> elasticsearch.yml
+echo "network.host: 0.0.0.0" >> elasticsearch.yml
+
+echo '#----transport module settings------' >> elasticsearch.yml
+echo "http.port : ${HTTP_PORT}" >> elasticsearch.yml
+
+echo '#----discovery module settings------' >> elasticsearch.yml
 echo "discovery.zen.minimum_master_nodes: ${MINIMUM_MASTER_NODES}" >> elasticsearch.yml
+
+
+echo '#----configure swapiness------------' >> elasticsearch.yml
+echo "bootstrap.memory_lock : true" >> elasticsearch.yml
+
 
 chown elasticsearch:elasticsearch elasticsearch.yml
 
