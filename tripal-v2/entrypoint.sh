d549b1abcc76a364f63f451d9cde66f302d7c002
MChen <ming.chen0919@gmail.com>
2017-09-25 00:01:43 -0400

    update entrypoint.sh

--- tripal-v2/entrypoint.sh
+++ tripal-v2/entrypoint.sh
@@ -9,11 +9,13 @@ echo
 
 ## start postgresql
 rm -rf /var/lib/pgsql/data/postmaster.pid
-sudo -u postgres pg_ctl start -D /var/lib/pgsql/data/
+sudo -u postgres pg_ctl start -D /var/lib/pgsql/data/ > /dev/null 2>&1
 echo
-echo "starting database ..."
+echo "Starting database..."
 echo
-sleep 60
+sleep 30
+echo "Run the following code to restart httpd if httpd is stopped"
+echo "/user/sbin/httpd"
 
 
 ## start a cluster with two nodes
--- tripal-v3/entrypoint.sh
+++ tripal-v3/entrypoint.sh
@@ -2,18 +2,21 @@
 
 ## start apache
 rm -f /usr/local/apache2/logs/httpd.pid
+/usr/sbin/httpd && sleep 5
 echo
 echo "starting httpd ..."
 echo
-/usr/sbin/httpd && sleep 5
 
 ## start postgresql
 rm -rf /var/lib/pgsql/data/postmaster.pid
-sudo -u postgres pg_ctl start -D /var/lib/pgsql/data/
+sudo -u postgres pg_ctl start -D /var/lib/pgsql/data/ > /dev/null 2>&1
 echo
-echo "starting database ..."
+echo "Starting database..."
 echo
-sleep 60
+sleep 30
+echo "Run the following code to restart httpd if httpd is stopped"
+echo "/user/sbin/httpd"
+
 
 ## start a cluster with two nodes
 /etc/init.d/elasticsearch_node-01 start && sleep 3 # port=9201
