18ff985bae19504f585c1621114caaa6578b3bdd
Ming Chen <ming.chen0919@gmail.com>
2018-01-19 15:38:19 -0500

    add galaxy

--- tripal-v3/entrypoint.sh
+++ tripal-v3/entrypoint.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-## start apache
-rm -f /usr/local/apache2/logs/httpd.pid
-/usr/sbin/httpd && sleep 5
-echo
-echo "starting httpd ..."
-echo
-
 ## start postgresql
 rm -rf /var/lib/pgsql/data/postmaster.pid
 sudo -u postgres pg_ctl start -D /var/lib/pgsql/data/ > /dev/null 2>&1
 echo
 echo "Starting database..."
-echo
-sleep 30
-echo "Run the following code to restart httpd if httpd is stopped"
-echo "/user/sbin/httpd"
+sleep 20
 
+
+# enable tripal galaxy
+drush en -y tripal_galaxy
 
 ## start a cluster with two nodes
 #/etc/init.d/elasticsearch_node-01 start && sleep 3 # port=9201
